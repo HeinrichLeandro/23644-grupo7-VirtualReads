2dd7d4386f887c2250805c78a7373b2c4f13b457
HeinrichLeandro <heinrich.leandro@gmail.com>
2023-11-13 19:07:44 -0300

    mejoras en las tarjetas

--- src/Components/Tarjeta.jsx
+++ src/Components/Tarjeta.jsx
@@ -4,31 +4,45 @@ import React from 'react'
 function Tarjeta() {
   return ( 
     <div className='conternedortarj'>
-        <div class="card mb-3" style={{maxwidth: "540px"}} className='tarjeta'>
+        <div class="card mb-3" className='tarjeta'>
             <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <img src="https://i.pinimg.com/736x/81/c0/8b/81c08b81cfd5d42f429d9047567ce16f.jpg" class="img-fluid rounded-start" alt="harry potter" />
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="card-body text-start">
                         <h5 class="card-title">Harry Potter y la piedra filosofal</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                        <p class="card-text" className='texto'><small class="text-body-secondary">Autor: J. K. Rowling</small></p>
+                        <p class="card-text" className='texto'><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic debitis aspernatur veniam optio doloremque quisquam a quasi obcaecati illo, dicta dolorem odit deserunt necessitatibus deleniti odio. Quis, ea dolores. Harum?</small></p>
+                        <p class="card-text" className='texto'><small>Editorial:</small></p>
+                        <p class="card-text" className='texto'><small>I.S.B.N:</small></p>
+                        <p class="card-text" className='texto'><small>Nro. de páginas:</small></p>
+                        <p class="card-text" className='texto'><small>Idioma:</small></p>
+                        <p class="card-text" className='texto'><small>Formato:</small></p>
+                        <p class="card-text" className='texto'><small>Clasificación:</small></p>
+                        <p class="card-text" className='texto'><small>Fecha de Publicación:</small></p>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="card mb-3" style={{maxwidth: "540px"}} className='tarjeta'>
+        <div class="card mb-3" className='tarjeta'>
             <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <img src="https://i.pinimg.com/736x/81/c0/8b/81c08b81cfd5d42f429d9047567ce16f.jpg" class="img-fluid rounded-start" alt="harry potter" />
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="card-body text-start">
                         <h5 class="card-title">Harry Potter y la piedra filosofal</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                        <p class="card-text" className='texto'><small class="text-body-secondary">Autor: J. K. Rowling</small></p>
+                        <p class="card-text" className='texto'><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic debitis aspernatur veniam optio doloremque quisquam a quasi obcaecati illo, dicta dolorem odit deserunt necessitatibus deleniti odio. Quis, ea dolores. Harum?</small></p>
+                        <p class="card-text" className='texto'><small>Editorial:</small></p>
+                        <p class="card-text" className='texto'><small>I.S.B.N:</small></p>
+                        <p class="card-text" className='texto'><small>Nro. de páginas:</small></p>
+                        <p class="card-text" className='texto'><small>Idioma:</small></p>
+                        <p class="card-text" className='texto'><small>Formato:</small></p>
+                        <p class="card-text" className='texto'><small>Clasificación:</small></p>
+                        <p class="card-text" className='texto'><small>Fecha de Publicación:</small></p>
                     </div>
                 </div>
             </div>
