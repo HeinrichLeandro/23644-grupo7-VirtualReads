b98f298df01c7b5476e1ae3446b34ed06caa28ca
HeinrichLeandro <heinrich.leandro@gmail.com>
2023-11-22 20:02:16 -0300

    retoques en los parametros de busqueda

--- src/Components/Buscador.jsx
+++ src/Components/Buscador.jsx
@@ -5,6 +5,7 @@ function Buscador({onBookData}) {
 const [search,setSearch]=useState("");
 const [genre, setGenre] = useState('Género');
 const [year, setYear] = useState('Año de publicación');
+const [year2, setYear2] = useState('Año de publicación2');
 const [author, setAuthor] = useState('Autor');
 const [language, setLanguage] = useState('Idioma');
 //Función para buscar los libros
@@ -12,11 +13,12 @@ function buscarLibros(){
     // var search = document.getElementById('searchInput').value;
     const apiUrl =
     'https://www.googleapis.com/books/v1/volumes?q=' +
-    search + 
+    search +
     (genre !== 'Género' ? `+subject:${genre}` : '')+
-    (year !== 'Año de publicación' ? `+publishedDate=${year}` : '') +
+    (year !== 'Año de publicación' ? `&before:1+ene+${year}` : '') +
+    (year2 !== 'Año de publicación2' ? `&after:1+ene+${year2}` : '') +
     (author !== 'Autor' ? `+inauthor:${author}` : '') +
-    (language !== 'Idioma' ? `+language:${language}` : '') +
+    (language !== 'Idioma' ? `&langRestrict=${language}` : '') +
     `&key=AIzaSyDfeBesAAxCA8CyF3ebH1-ea_wYUna70rQ&maxResults=2`;
 
     //  +
@@ -44,6 +46,39 @@ function buscarLibros(){
       
     
     };
+
+    function busquedaRapida(){
+        // var search = document.getElementById('searchInput').value;
+        const apiUrl =
+        'https://www.googleapis.com/books/v1/volumes?q=' +
+        search +
+        `&key=AIzaSyDfeBesAAxCA8CyF3ebH1-ea_wYUna70rQ&maxResults=2`;
+    
+        //  +
+        // (publisher !== 'Editorial' ? `&inpublisher=${publisher}` : '') +
+        // (year !== 'Año de publicación' ? `&publishedDate=${year}` : '') +
+        // (isbn !== 'Codigo ISBN' ? `&isbn=${isbn}` : '') +
+        // (author !== 'Autor' ? `&inauthor=${author}` : '') +
+        // (language !== 'Idioma' ? `&language=${language}` : '') +
+        // (edition !== 'Edición' ? `&edition=${edition}` : '');
+    
+        fetch(apiUrl)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Error al obtener los datos.');
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log('Respuesta de la API:', data);
+            onBookData(data.items);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+          
+        
+        };
   return ( 
     <div className='buscador'>
         <nav class="navbar">
@@ -51,7 +86,7 @@ function buscarLibros(){
                 <img src="https://images.vexels.com/media/users/3/143466/isolated/preview/b47bfb19d11e66c3be00ccb0632047ce-lupa-simple.png" class="img-fluid" width="30" height="30" />
                 <form class="d-flex mt-3" role="search">
                     <input id='searchInput' class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" value={search} onChange={e=>setSearch(e.target.value)}/>
-                    <button class="btn btn-outline-success" onClick={buscarLibros}>Buscar</button>
+                    <button class="btn btn-outline-success" onClick={busquedaRapida}>Buscar</button>
                 </form>
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBuscador" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span ><img src="https://cdn-icons-png.flaticon.com/512/6526/6526846.png" class="img-fluid"  width="30" height="30"/></span>
@@ -62,7 +97,10 @@ function buscarLibros(){
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">                            
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3"> 
+
+                            <input id='searchInput' class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" value={search} onChange={e=>setSearch(e.target.value)}/>                           
+                            <br/>
 
                             <select class="form-select" aria-label="Default select example"onChange={(e) => setGenre(e.target.value)} >
                                 <option selected>Género</option>
@@ -81,8 +119,17 @@ function buscarLibros(){
                             </select>
                             <br/>
 
+                            <select class="form-select" aria-label="Default select example" onChange={(e) => setYear2(e.target.value)}>
+                                <option selected>Año de publicación inicial</option>
+                                <option value="2000">2000</option>
+                                <option value="2001">2001</option>
+                                <option value="2002">2002</option>
+                                <option value="2003">2003</option>
+                            </select>
+                            <br/>
+
                             <select class="form-select" aria-label="Default select example" onChange={(e) => setYear(e.target.value)}>
-                                <option selected>Año de publicación</option>
+                                <option selected>Año de publicación final</option>
                                 <option value="2000">2000</option>
                                 <option value="2001">2001</option>
                                 <option value="2002">2002</option>
@@ -125,7 +172,6 @@ function buscarLibros(){
 
                         </ul>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                        <button type="button" class="btn btn-secondary">Cancelar</button>
                         <button type="button" class="btn btn-primary" onClick={buscarLibros}>Aplicar</button>
                         </div>
       </div>
--- src/Components/Tarjeta.jsx
+++ src/Components/Tarjeta.jsx
@@ -8,13 +8,13 @@ function Tarjeta({ book}) {
         <div key={index} className={`card mb-3 tarjeta${index + 1}`}>
           <div className="row g-0">
             <div className="col-md-6">
-              <img src={libro.volumeInfo.imageLinks?.thumbnail || 'URL_IMAGEN_POR_DEFECTO'} className="img-fluid rounded-start" alt={libro.volumeInfo.title} />
+              <img src={libro.volumeInfo.imageLinks?.thumbnail || 'URL_IMAGEN_POR_DEFECTO'} className="img-fluid rounded-start imgLibro" alt={libro.volumeInfo.title} />
             </div>
             <div className="col-md-6">
               <div className="card-body text-start">
                 <h5 className="card-title">{libro.volumeInfo.title}</h5>
                 <p className="card-text texto"><small className="text-body-secondary">Autor: {libro.volumeInfo.authors?.[0] || 'Desconocido'}</small></p>
-                <p className="card-text texto"><small>{libro.volumeInfo.description || 'Descripción no disponible'}</small></p>
+                {/* <p className="card-text texto"><small>{libro.volumeInfo.description || 'Descripción no disponible'}</small></p> */}
                 <p className="card-text texto"><small>Editorial: {libro.volumeInfo.publisher || 'Desconocido'}</small></p>
                 <p className="card-text texto"><small>I.S.B.N: {libro.volumeInfo.industryIdentifiers?.[0]?.identifier || 'Desconocido'}</small></p>
                 <p className="card-text texto"><small>Nro. de páginas: {libro.volumeInfo.pageCount || 'Desconocido'}</small></p>
