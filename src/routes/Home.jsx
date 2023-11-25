e258aec7d31abc69289a29389b140fba1a343963
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2023-11-24 22:36:07 -0300

    vista home

---
+++ src/routes/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Navbar from '../Components/Navbar';
+import Buscador from '../Components/Buscador';
+import Tarjeta from '../Components/Tarjeta';
+import Recomendados from '../Components/Recomendados';
+
+function Home() {
+  const [bookData, setBookData] = useState([]);
+  // Función para recibir los datos de búsqueda desde Buscador
+  const handleBookData = (data) => {
+    setBookData(data);
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <Buscador onBookData={handleBookData} />
+
+      <Recomendados />
+
+      <Tarjeta book={bookData} />
+    </div>
+  )
+}
+
+export default Home;
