79366bd30b4411bc581d806d5764ac99b5578d6f
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2023-11-29 20:09:54 -0300

    Agregue rutas y movi los componentes a Home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import Buscador from './Components/Buscador';
-import Tarjeta from './Components/Tarjeta';
-import Recomendados from './Components/Recomendados';
-import React, { useState } from 'react';
+import React from 'react';
+import { AuthContextProvider } from './context/AuthContext';
+import { MisRutas } from './routes/routes';
 
 function App() {
-  const [bookData, setBookData] = useState([]);
-
-  // Función para recibir los datos de búsqueda desde Buscador
-  const handleBookData = (data) => {
-    setBookData(data);
-  };
+  
   return (
-    <div className="App">
-      
-    <Navbar/>
-
-    <Buscador onBookData={handleBookData}/>
-
-    <Recomendados/>
-        
-    <Tarjeta book={bookData}/>
-    
-    </div>
+      <AuthContextProvider>
+        <div>
+          <MisRutas />
+        </div>
+      </AuthContextProvider>
   );
 }
 
