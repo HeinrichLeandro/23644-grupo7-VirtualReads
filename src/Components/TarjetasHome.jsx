af91611469337d87faa1dc7dfb2d468d2173c90b
HeinrichLeandro <heinrich.leandro@gmail.com>
2023-12-11 16:25:52 -0300

    cambiados algunos estilos

--- src/Components/TarjetasHome.jsx
+++ src/Components/TarjetasHome.jsx
@@ -10,11 +10,11 @@ function TarjetasHome({book, onCardClick}) {
         <div key={index} className={`card mb-3 tarjeta${index + 1}`} >
           <div className="row g-0 tarjHome">
             <div className="col-md-4">
-              <Link to={`/books/${index + 1}`}><img src={libro.volumeInfo.imageLinks?.thumbnail || 'URL_IMAGEN_POR_DEFECTO'} className="img-fluid rounded-start imgLibro" alt={libro.volumeInfo.title} onClick={() => onCardClick(libro)} /></Link>
+              <Link to={`/books/${index + 1}`} style={{textDecoration: 'none'}}><img src={libro.volumeInfo.imageLinks?.thumbnail || 'URL_IMAGEN_POR_DEFECTO'} className="img-fluid rounded-start imgLibro" alt={libro.volumeInfo.title} onClick={() => onCardClick(libro)} /></Link>
             </div>
             <div className="col-md-4">
               <div className="card-body text-start">
-                <Link to={`/books/${index + 1}`}><h5 className="card-title" onClick={() => onCardClick(libro)}>{libro.volumeInfo.title}</h5></Link>
+                <Link to={`/books/${index + 1}`} style={{textDecoration: 'none' , color: 'black'}}><h5 className="card-title" onClick={() => onCardClick(libro)}>{libro.volumeInfo.title}</h5></Link>
                 <p className="card-text texto"><small className="text-body-secondary">Autor: {libro.volumeInfo.authors?.[0] || 'Desconocido'}</small></p>
                 {/* <p className="card-text texto"><small>{libro.volumeInfo.description || 'Descripción no disponible'}</small></p> */}
                 <p className="card-text texto"><small>Editorial: {libro.volumeInfo.publisher || 'Desconocido'}</small></p>
